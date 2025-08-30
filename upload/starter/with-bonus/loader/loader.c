3e7df3ee2651b22e41e1106d3355696d223bbd13
Krypton018 <bhavyay958@gmail.com>
2025-08-30 17:12:16 +0530

    loader

--- upload/starter/with-bonus/loader/loader.c
+++ upload/starter/with-bonus/loader/loader.c
@@ -17,7 +17,7 @@ void loader_cleanup() {
  * Load and run the ELF executable file
  */
 void load_and_run_elf(char** exe) {
-    fd = open(argv[1], O_RDONLY);
+    fd = open(exe[1], O_RDONLY);
     if (fd < 0){
         perror("couldn't open file");
         exit(1);
@@ -36,6 +36,9 @@ void load_and_run_elf(char** exe) {
         exit(1);
     }
 
+    // Virual address of the entry point
+    unsigned int entry_virtual_add = ehdr->e_entry;
+
     // Loading the entire program header table
     phdr = (Elf32_Phdr*)malloc(sizeof(Elf32_Phdr) * ehdr->e_phnum);
     // Offsetting to start of program header
@@ -43,7 +46,7 @@ void load_and_run_elf(char** exe) {
         perror("lseek to program header table failed");
         exit(1);
     }
-    if (read(fd, phdr, sizeof(phdr) != sizeof(phdr))) {
+    if (read(fd, phdr, sizeof(Elf32_Phdr) * ehdr->e_phnum) != sizeof(Elf32_Phdr) * ehdr->e_phnum) {
         perror("phdr is faulty");
         exit(1);
     }
@@ -51,14 +54,59 @@ void load_and_run_elf(char** exe) {
 
     // 2. Iterate through the PHDR table and find the section of PT_LOAD 
     //    type that contains the address of the entrypoint method in fib.c
+    Elf32_Phdr *ph;
+    for (int i=0; i<ehdr->e_phnum; i++) {
+        if (phdr[i].p_type == PT_LOAD) {
+            unsigned int start_add = phdr[i].p_vaddr;
+            unsigned int end_add = start_add + phdr[i].p_memsz;
 
+            if (start_add<=entry_virtual_add && entry_virtual_add<end_add) {
+                ph = &phdr[i];
+                break;
+            }
+        }
+    }
 
     
     // 3. Allocate memory of the size "p_memsz" using mmap function 
     //    and then copy the segment content
+    void *segment_virtual_mem;
+    segment_virtual_mem = mmap(NULL, ph->p_memsz, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_ANONYMOUS|MAP_PRIVATE, 0, 0);
+    if (segment_virtual_mem == MAP_FAILED) {
+        perror("mmap failed");
+        exit(1);
+    }
+    
+    if (lseek(fd, ph->p_offset, SEEK_SET) < 0) {
+        perror("lseek to program header failed");
+        exit(1);
+    }
+    if (read(fd, segment_virtual_mem, ph->p_memsz) != ph->p_memsz) {
+        perror("PT_LOAD segment is faulty");
+        exit(1);
+    }
+
     // 4. Navigate to the entrypoint address into the segment loaded in the memory in above step
+    unsigned int entry_offset = entry_virtual_add - ph->p_vaddr;
+    unsigned int entry_address = (unsigned int)segment_virtual_mem + entry_offset;
+    
     // 5. Typecast the address to that of function pointer matching "_start" method in fib.c.
+    int (*_start)() = (int(*)()) (entry_address);
+
     // 6. Call the "_start" method and print the value returned from the "_start"
     int result = _start();
     printf("User _start return value = %d\n",result);
+}
+
+int main(int argc, char **argv){
+
+    if (argc != 2){
+        printf("usage: %s binary_file\n", argv[0]);
+        return 1;
+    }
+
+    load_and_run_elf(argv);
+    loader_cleanup();
+
+    return 0;
 }
